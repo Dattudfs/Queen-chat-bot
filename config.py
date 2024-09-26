fa95ad3a8cb96ea83ab259e9ef2c6a4aa1bdace3
Dattudfs <vimalif28@gmail.com>
2024-09-26 18:55:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_ID = "28362850"
 API_HASH = "34f9cb93364db16fc45d003e4c81d97a"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", "6966314106:AAFtyEd7_WxgV7hKmfA6u7gzwV5FFVpLuW0")
-MONGO_URL = getenv("MONGO_URL", "
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority"
 SUPPORT_GRP = "DFSchinnaop"
 UPDATE_CHNL = "DFSchinnaop"
 OWNER_USERNAME = "DFSchinnaop"
